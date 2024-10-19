60e74d40410ba3e8d91e79f25e318577c10e7d4e
jalpp <jalp.panchal44@gmail.com>
2024-10-18 23:38:08 -0400

    feat(1password): add 1password integration

--- style/style.go
+++ style/style.go
@@ -6,4 +6,5 @@ var (
 	GreenStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#7DDA58"))
 	ErrorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#E4080A"))
 	VaultStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFDE59"))
+	OPassStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#4CAAF7"))
 )
